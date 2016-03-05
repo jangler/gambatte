9f5a8ed562e6a89b9898bc9c8b121aef92d1523e
Brandon Mulcahy <brandon@jangler.info>
2016-03-05 06:06:37 -0500

    gambatte_sdl: increase joystick axis tolerances.

--- gambatte_sdl/src/gambatte_sdl.cpp
+++ gambatte_sdl/src/gambatte_sdl.cpp
@@ -691,9 +691,9 @@ bool GambatteSdl::handleEvents(BlitterWrapper &blitter) {
 	case SDL_JOYAXISMOTION:
 		jd.dev_num = e.jaxis.which;
 		jd.num = e.jaxis.axis;
-		jd.dir = e.jaxis.value < -8192
+		jd.dir = e.jaxis.value < -16384
 		       ? JoyData::dir_down
-		       : (e.jaxis.value > 8192 ? JoyData::dir_up : JoyData::dir_centered);
+		       : (e.jaxis.value > 16384 ? JoyData::dir_up : JoyData::dir_centered);
 
 		for (std::pair<jmap_t::iterator, jmap_t::iterator> range =
 				jaMap.equal_range(jd); range.first != range.second; ++range.first) {
